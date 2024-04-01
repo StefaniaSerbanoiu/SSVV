767c5806f03d815fad7dc489a9ca3b82b876b34a
916-Serbanoiu-Stefania <91834791+916-Serbanoiu-Stefania@users.noreply.github.com>
2024-04-01 19:26:36 +0300

    update AppTest

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,76 +1,59 @@
 package org.example;
 
 import domain.Student;
-import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import repository.StudentXMLRepo;
 import service.Service;
 import validation.StudentValidator;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  * Unit test for simple App.
  */
-public class AppTest extends TestCase {
+public class AppTest{
 
     private Service service;
     private StudentXMLRepo studentRepo;
 
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest(String testName) {
-        super(testName);
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite(AppTest.class);
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    @org.junit.Test
-    public void testApp() {
-        assertTrue(true);
-    }
-
-    @Before
+    @BeforeEach
     public void setUp() {
         studentRepo = new StudentXMLRepo("src/resources/Studenti2.xml");
         StudentValidator studentValidator = new StudentValidator();
         service = new Service(studentRepo, studentValidator, null, null, null, null);
     }
 
-    @org.junit.Test
+    @org.junit.jupiter.api.Test
     public void testAddStudent_Success() {
+
+        Student result;
         Student student = new Student("1", "John Doe", 123, "john.doe@example.com");
 
-        Student result = service.addStudent(student);
-
+        result = service.addStudent(student);
         assertEquals(student, result);
 
-        service.deleteStudent(student.getID());
+        service.deleteStudent("1");
     }
 
-    @org.junit.Test
+    @Test
     public void testAddStudent_Failure_ExistingStudent() {
+
         Student student = new Student("1", "John Doe", 123, "john.doe@example.com");
 
         Student result;
 
-        result = service.addStudent(student);
+        result = service.findStudent("1");
+        System.out.println(result);
+
+        result = service.addStudent(student); // Add the student once
         assertEquals(student, result);
-
         result = service.addStudent(student);
-        assertNull(result);
+        assertEquals(null, result);
 
-        service.deleteStudent(student.getID());
+        service.deleteStudent("1");
     }
 }
